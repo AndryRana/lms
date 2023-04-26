7de18443a1ed42d9c39a1258196759fcf0cc86a9
AndryRana <lsourcus@gmail.com>
2023-04-26 15:50:28 +0200

    add env

--- lms/settings.py
+++ lms/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5b2!+8fc8fjpf=p)h&y1nf63*)qd279dpn((i54#&t$g$to0kn'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
